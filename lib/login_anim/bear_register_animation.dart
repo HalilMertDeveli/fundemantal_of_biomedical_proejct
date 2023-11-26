d9193b9025eda5f04f8ffb04b508fd9dfe60ecac
haliko <halilmertdeveliii@gmail.com>
2023-11-26 22:14:03 +0300

    i added validator for input areas

--- lib/login_anim/bear_register_animation.dart
+++ lib/login_anim/bear_register_animation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fundemetals_of_biomedical/util/util.dart';
 import 'package:rive/rive.dart';
 
 class BearRegisterAnimation extends StatefulWidget {
@@ -12,6 +13,7 @@ class _BearRegisterAnimationState extends State<BearRegisterAnimation> {
   String _name = '';
   String _email = '';
   DateTime selectedDate = DateTime.now();
+  String ? _password = '';
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -20,7 +22,7 @@ class _BearRegisterAnimationState extends State<BearRegisterAnimation> {
       firstDate: DateTime(1900),
       lastDate: DateTime.now(),
     );
-  
+
     if (picked != null && picked != selectedDate) {
       setState(() {
         selectedDate = picked;
@@ -28,180 +30,200 @@ class _BearRegisterAnimationState extends State<BearRegisterAnimation> {
     }
   }
 
-    var animationLink = 'assets/login.riv';
+  var animationLink = 'assets/login.riv';
 
-    Artboard? artboard;
+  Artboard? artboard;
 
-    late StateMachineController? stateMachineController;
+  late StateMachineController? stateMachineController;
 
-    late SMITrigger failTrigger, successTrigger;
+  late SMITrigger failTrigger, successTrigger;
 
-    late SMIBool isChecking, isHandsUp;
+  late SMIBool isChecking, isHandsUp;
 
-    late SMINumber lookNum;
+  late SMINumber lookNum;
 
-    void _submitForm() {
-      if (_formKey.currentState!.validate()) {
-        _formKey.currentState!.save();
-        print('Name: $_name');
-        print('Email: $_email');
-      }
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Name: $_name');
+      print('Email: $_email');
     }
+  }
 
-    initArtboard() {
-      rootBundle.load(animationLink).then((value) {
-        final file = RiveFile.import(value);
-        final art = file.mainArtboard;
+  initArtboard() {
+    rootBundle.load(animationLink).then((value) {
+      final file = RiveFile.import(value);
+      final art = file.mainArtboard;
 
-        stateMachineController =
-            StateMachineController.fromArtboard(art, "Login Machine")!;
+      stateMachineController =
+          StateMachineController.fromArtboard(art, "Login Machine")!;
 
-        if (stateMachineController != null) {
-          art.addController(stateMachineController!);
-          for (var element in stateMachineController!.inputs) {
-            //print(element); -> we got animation trigger
-            if (element.name == "isChecking") {
-              isChecking = element as SMIBool;
-            } else if (element.name == "isHandsUp") {
-              isHandsUp = element as SMIBool;
-            } else if (element.name == "trigSuccess") {
-              successTrigger = element as SMITrigger;
-            } else if (element.name == "trigFail") {
-              failTrigger = element as SMITrigger;
-            } else if (element.name == 'numLook') {
-              lookNum = element as SMINumber;
-            }
+      if (stateMachineController != null) {
+        art.addController(stateMachineController!);
+        for (var element in stateMachineController!.inputs) {
+          //print(element); -> we got animation trigger
+          if (element.name == "isChecking") {
+            isChecking = element as SMIBool;
+          } else if (element.name == "isHandsUp") {
+            isHandsUp = element as SMIBool;
+          } else if (element.name == "trigSuccess") {
+            successTrigger = element as SMITrigger;
+          } else if (element.name == "trigFail") {
+            failTrigger = element as SMITrigger;
+          } else if (element.name == 'numLook') {
+            lookNum = element as SMINumber;
           }
         }
-        setState(() {
-          artboard = art;
-        });
-      });
-    }
-
-    @override
-    void initState() {
-      super.initState();
-      initArtboard();
-    }
-
-    checking() {
-      isHandsUp.change(false);
-      isChecking.change(true);
-      lookNum.change(0);
-    }
-
-    moveEyes(value) {
-      lookNum.change(value.length.toDouble());
-    }
-
-    handsUp() {
-      isHandsUp.change(true);
-      isChecking.change(false);
-    }
-
-    login() {
-      isHandsUp.change(false);
-      isChecking.change(false);
-      if (_email == "admin" || _name == 'admin') {
-        successTrigger.fire();
-      } else {
-        failTrigger.fire();
       }
-    }
+      setState(() {
+        artboard = art;
+      });
+    });
+  }
 
-   
-  
-  
+  @override
+  void initState() {
+    super.initState();
+    initArtboard();
+  }
+
+  checking() {
+    isHandsUp.change(false);
+    isChecking.change(true);
+    lookNum.change(0);
+  }
+
+  moveEyes(value) {
+    lookNum.change(value.length.toDouble());
+  }
+
+  handsUp() {
+    isHandsUp.change(true);
+    isChecking.change(false);
+  }
+
+  login() {
+    isHandsUp.change(false);
+    isChecking.change(false);
+    if (_email == "admin" || _name == 'admin') {
+      successTrigger.fire();
+    } else {
+      failTrigger.fire();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if (artboard != null) {
-        return Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 400,
-                  height: 350,
-                  child: Rive(
-                    artboard: artboard!,
-                  ),
+      return Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 400,
+                height: 350,
+                child: Rive(
+                  artboard: artboard!,
                 ),
-                TextFormField(
-                  onTap: checking, //move eyes
-                  onChanged: ((value) => moveEyes(value)), //move eyes
-                  decoration: InputDecoration(
-                    labelText: 'Email adresinizi giriniz :',
-                    labelStyle: TextStyle(fontSize: 20, color: Colors.black),
-                    border: OutlineInputBorder(),
-                  ),
-                  onSaved: (value) {
-                    _name = value!;
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
+              ),
+              CustomTextField(
+                hint: "İsminizi giriniz",
+                icon: Icon(Icons.verified_user),
+                onSaved: (String? input) {
+                  _password = input;
+                },
+                validator: (String ? value ){
+                  return validateUserName(value);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onTap: checking,
+                //move eyes
+                onChanged: ((value) => moveEyes(value)),
+                //move eyes
+                decoration: InputDecoration(
+                  labelText: 'Email adresinizi giriniz :',
+                  labelStyle: TextStyle(fontSize: 20, color: Colors.black),
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  onTap: handsUp,
-                  decoration: InputDecoration(
-                      labelText: 'Şifrenizi giriniz :',
-                      labelStyle:
-                          TextStyle(fontSize: 20.0, color: Colors.black),
-                      border: OutlineInputBorder()),
-                  onSaved: (value) {
-                    _email = value!;
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-                //date time
-                Text(
-                  'Seçilen tarih:',
-                  style: TextStyle(fontSize: 18),
-                ),
-                Text(
-                  '${selectedDate.toLocal()}'.split(' ')[0],
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => _selectDate(context),
-                  child: Text('Doğum tarihinizi seçiniz'),
-                ),
-                //end of region date time
-                ElevatedButton(
-                  onPressed: () {
-                    _submitForm();
-                    login();
-                    print(selectedDate);
-                  },
-                  child: Text('Login'),
-                ),
-              ],
-            ),
+                onSaved: (value) {
+                  _name = value!;
+                },
+                validator: (value) {
+                  return validateEmailAddress(value);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onTap: handsUp,
+                decoration: InputDecoration(
+                    labelText: 'Şifrenizi giriniz :',
+                    labelStyle: TextStyle(fontSize: 20.0, color: Colors.black),
+                    border: OutlineInputBorder()),
+                onSaved: (value) {
+                  _email = value!;
+                },
+                validator: (value) {
+                  return validatePassword(value);
+                },
+              ),
+              //date time
+              Text(
+                'Seçilen tarih:',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                '${selectedDate.toLocal()}'.split(' ')[0],
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () => _selectDate(context),
+                child: Text('Doğum tarihinizi seçiniz'),
+              ),
+              //end of region date time
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()){
+                    _formKey.currentState!.save();
+                    print("it is wokring");
+                  }
+                  _submitForm();
+                  login();
+                  print(selectedDate);
+                },
+                child: Text('Login'),
+              ),
+              filledButton(
+                "Kullanıcı Oluştur",
+                Colors.white38,
+                Colors.black12,
+                Colors.black45,
+                Colors.white,
+                () {},
+              )
+            ],
           ),
-        );
-      } else {
-        return SizedBox(
-          width: 400,
-          height: 200,
-          child: Container(color: Colors.white),
-        );
-      }
-  
+        ),
+      );
+    } else {
+      return SizedBox(
+        width: 400,
+        height: 200,
+        child: Container(color: Colors.white),
+      );
+    }
   }
+
+
+
 }
--- lib/util/util.dart
+++ lib/util/util.dart
@@ -8,3 +8,114 @@ void navigatePage(BuildContext context, Widget pageName) {
     ),
   );
 }
+class CustomTextField extends StatelessWidget {
+  CustomTextField(
+      {required this.icon,
+        required this.hint,
+        this.obsecure = false,
+        required this.validator,
+        required this.onSaved});
+  final FormFieldSetter<String> onSaved;
+  final Icon icon;
+  final String hint;
+  final bool obsecure;
+  final FormFieldValidator<String> validator;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: TextFormField(
+        onSaved: onSaved,
+        validator: validator,
+        autofocus: true,
+        obscureText: obsecure,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        decoration: InputDecoration(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            hintText: hint,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 2,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 3,
+              ),
+            ),
+            prefixIcon: Padding(
+              child: IconTheme(
+                data: IconThemeData(color: Theme.of(context).primaryColor),
+                child: icon,
+              ),
+              padding: EdgeInsets.only(left: 30, right: 10),
+            )),
+      ),
+    );
+  }
+}
+Widget filledButton(String text, Color splashColor, Color highlightColor,
+    Color fillColor, Color textColor, void function()) {
+  return MaterialButton(
+    highlightElevation: 0.0,
+    splashColor: splashColor,
+    highlightColor: highlightColor,
+    elevation: 0.0,
+    color: fillColor,
+    shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(30.0)),
+    child: Text(
+      text,
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: textColor, fontSize: 20),
+    ),
+    onPressed: () {
+      function();
+    },
+  );
+}
+String? validatePassword(String? value) {
+  if (value!.isEmpty) {
+    return 'Lütfen bir Şifre giriniz';
+  }
+  if (value.length < 8) {
+    return "Şifre 8 karakterden düşük olamaz";
+  }
+  if (value =="")
+    return "Lütfen bir şifre sağlayın";
+  if (!value.contains(RegExp(r'[a-zA-Z]'))) {
+    return "Şifrende en az bir büyük harf ve bir küçük harfe sahip olmalıdır";
+  }
+  if (!value.contains(RegExp(r'[0-9]'))) {
+    return "Şifrende en az bir tane sayı olması gerekmektedir";
+  }
+  return null;
+}
+String? validateUserName(String ? value){
+  if (value!.isEmpty) {
+    return 'Lütfen bir isim giriniz';
+  }
+  return null;
+}
+String ? validateEmailAddress(String ? value){
+
+  final RegExp emailRegExp =
+  RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+  if (value == null || value.isEmpty) {
+    return 'Please enter an email address';
+
+  }
+
+  if(!emailRegExp.hasMatch(value)){
+    return "Uygun bir email adresi giriniz";
+  }
+  else {
+    return null;
+  }
+}
