4f82bf33948ab36d667b4b0c61732fae4fb3b801
HMDSS <halilmertdeveliii@gmail.com>
2024-03-07 05:36:05 +0300

    i arranged the login options for social media for

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,13 +6,108 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Image.asset(
-          "lib/assets/img/splash/Splash2x.png",
-          fit: BoxFit.fill,
-        ),
+      appBar: AppBar(
+        title: const Text("Hello world"),
+      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment(0.1, 0.6),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("Clicked to google");
+                  },
+                  child: Card(
+                    elevation: 10,
+                    child: Container(
+                      margin: EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      padding: EdgeInsets.only(
+                        left: 15,
+                        right: 15,
+                        top: 8,
+                        bottom: 8,
+                      ), // Add padding to create space for the image
+                      child: Container(
+                        child: Image.asset(
+                          "lib/assets/icons/login_singup_icons/google.png",
+                          width: 80,
+                          height: 45,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print("Cliked facebook");
+                  },
+                  child: Card(
+                    elevation: 10,
+                    child: Container(
+                      margin: EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      padding: EdgeInsets.only(
+                        left: 15,
+                        right: 15,
+                        top: 8,
+                        bottom: 8,
+                      ), // Add padding to create space for the image
+                      child: Container(
+                        child: Image.asset(
+                          "lib/assets/icons/login_singup_icons/facebook (1).png",
+                          width: 80,
+                          height: 45,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print("Clicked twitter");
+                  },
+                  child: Card(
+                    elevation: 10,
+                    child: Container(
+                      margin: EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      padding: EdgeInsets.only(
+                        left: 15,
+                        right: 15,
+                        top: 8,
+                        bottom: 8,
+                      ), // Add padding to create space for the image
+                      child: Container(
+                        child: Image.asset(
+                          "lib/assets/icons/login_singup_icons/twitter.png",
+                          width: 80,
+                          height: 45,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
